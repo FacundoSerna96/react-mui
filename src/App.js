b44bcb1842918548ec89f9396acfbe153154697a
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-04-30 02:35:03 -0300

    Implementación del Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
+import Rightbar from './components/Rightbar'
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material'
+import Navbar from './components/Navbar'
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [mode, setMode] = useState('light')
+
+  const darkTheme = createTheme({
+    palette: {
+      mode
+    }
+  })
   return (
-    <div className="app">
-      
-    </div>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor='background.default' color='text.primary'>
+        <Navbar />
+        <Stack direction='row' spacing={2} justifyContent='space-between'>
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
